54e843796656adf58f9476f730aa2ea8c88f7bd9
Mark Gerrard <limptwiglet@gmail.com>
2012-04-03 14:19:38 +0100

    Got computed properties working, need to add caching and binding stuff

--- gelatin.js
+++ gelatin.js
@@ -4,15 +4,59 @@
 	// Setup our Gelatin namespace
 	var Gelatin = root.Gelatin = {};
 
+	var get = Gelatin.get = function (obj, key) {
+		var value = obj[key];
 
-	// Gelatin base class
-	Gelatin.Base = new Class({
-		get: function () {
+		if (typeOf(value) === 'object') {
+			value = value.get(obj, key);
+		}
+
+		return value;
+	};
+
+
+	Gelatin.set = function (obj, key, value) {
+		var prop = obj[key];
+
+		if (typeOf(prop) === 'object') {
+			return prop.set(obj, key, value);
+		}
+
+		obj[key] = value;
+	};
+
+
+	Gelatin.ComputedProperty = new Class({
+		Implements: [Options],
+
+		options: {
 
 		},
 
-		set: function () {
+		initialize: function (func, options) {
+			this.setOptions(options);
+			this.func = func;
 
+			return this;
+		},
+
+		property: function () {
+			this.props = Array.from(arguments);
+			return this;
+		},
+
+		get: function (obj, key) {
+			var ret = this.func.call(obj, key);
+			return ret;
+		},
+
+		set: function (obj, key, value) {
+			this.func.call(obj, key, value);			
 		}
 	});
+
+	Gelatin.computed = function () {
+		var computed = new Gelatin.ComputedProperty(arguments[0]);
+		return computed;
+	};
 })(this);
--- test/tests/global.js
+++ test/tests/global.js
@@ -1,12 +1,50 @@
 var expect = chai.expect;
 
 describe('global gelatin helpers and classes', function () {
-	describe('BaseClass', function () {
-		it('should have get and set methods', function (done) {
-			var base = new Gelatin.Base({});
+	describe('get', function () {
+		var obj = {
+			fname: 'Mark',
+			lname: 'Gerrard',
+			fullName: Gelatin.computed(function () {
+				return Gelatin.get(this, 'fname') + ' ' + Gelatin.get(this, 'lname');
+			}).property('fname', 'lname')
+		};
 
-			expect(base).to.have.property('get');
-			expect(base).to.have.property('set');
+		it('should return property', function (done) {
+			expect(Gelatin.get(obj, 'fname')).to.eql(obj.fname);
+			expect(Gelatin.get(obj, 'burr')).to.not.exist;
+			done();	
+		});
+
+		it('should return value from a computed property', function (done) {
+			expect(Gelatin.get(obj, 'fullName')).to.eql(obj.fname + ' ' + obj.lname);
+			done();
+		});
+	});
+
+	describe('set', function () {
+		var obj = {
+			fullName: Gelatin.computed(function (key, value) {
+				if (arguments.length > 1) {
+					var parts = value.split(' ');
+					Gelatin.set(this, 'fname', parts[0]);
+					Gelatin.set(this, 'lname', parts[1]);
+				}
+
+				return Gelatin.get(this, 'fname') + ' ' + Gelatin.get(this, 'lname');
+			}).property('fname', 'lname')
+		};
+
+		it('should set a property', function (done) {
+			Gelatin.set(obj, 'fname', 'Mark');
+			expect(obj.fname).to.eql('Mark');
+			done();
+		});
+
+		it('computed property should set properties', function (done) {
+			Gelatin.set(obj, 'fullName', 'Mark Gerrard');
+			expect(obj.fname).to.eql('Mark');
+			expect(obj.lname).to.eql('Gerrard');
 			done();
 		});
 	});
