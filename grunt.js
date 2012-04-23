b2eaf73d4eb7db069d09b565662934d4c6d1f084
Mark Gerrard <limptwiglet@gmail.com>
2012-04-23 17:59:16 +0100

    Started splitting project source files so they can be built using grunt

---
+++ grunt.js
@@ -0,0 +1,32 @@
+module.exports = function (grunt) {
+	var FILES = [
+			'<%= dirs.src %>core/get.js',
+			'<%= dirs.src %>core/set.js',
+			'<%= dirs.src %>core/object.js'
+		];
+
+	grunt.initConfig({
+		pkg: '<json:package.json>',
+
+		dirs: {
+			src: 'src/',
+			dest: 'dist/'
+		},
+
+		lint: {
+			all: FILES
+		},
+
+		concat: {
+			dist: {
+				src: FILES,
+				dest: '<%= dirs.dest %>gelatin-<%= pkg.version %>.js'
+			}
+		}
+	});
+
+
+	grunt.registerTask('default', 'lint');
+
+	grunt.registerTask('build', 'concat:dist');
+};
